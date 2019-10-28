85750c25ec372af3774666fae314cf2f728dc115
Danil Ilyshuk <danil.ilyashuk@gmail.com>
2019-10-28 23:20:53 +0300

    Changed downloaded audio format to wav

--- audio_downloader.py
+++ audio_downloader.py
@@ -6,7 +6,7 @@ ydl_opts = {
     'format': 'bestaudio/best',
     'postprocessors': [{
         'key': 'FFmpegExtractAudio',
-        'preferredcodec': 'mp3',
+        'preferredcodec': 'wav',
         'preferredquality': '192',
     }],
 }
@@ -16,10 +16,8 @@ playlists = [
     'https://www.youtube.com/watch?v=4pXf9f37btE&list=PLp2aUaz0EAxCBeTw-kcLLERrF4wBZKjJv',
     'https://www.youtube.com/watch?v=CFBJG2ksvqo&list=PL8EJzNcJZNp0Uq-r2sMcL5nHyI-Po0Tjq',
     'https://www.youtube.com/watch?v=uCm4TK2Hggg&list=PLQTGSfnaYlCuonYGidCFg1aqii9a6cqL4'
-
 ]
 
 
 with youtube_dl.YoutubeDL(ydl_opts) as ydl:
     ydl.download(playlists)
-    
