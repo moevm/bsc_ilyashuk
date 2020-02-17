ddd02aa06a573ba0f126bfd0f3089541284e43f8
cupofme <danil.ilyashuk@gmail.com>
2020-02-17 23:25:26 +0300

    Add pause collecting and audio play

--- test_stand/app/src/main/java/com/cupofme/teststand/SegmentationActivity.kt
+++ test_stand/app/src/main/java/com/cupofme/teststand/SegmentationActivity.kt
@@ -1,16 +1,20 @@
 package com.cupofme.teststand
 
-import android.media.AudioFormat
-import android.media.AudioRecord
-import android.media.MediaRecorder
+import android.media.*
+import android.media.AudioFormat.*
+import android.media.AudioTrack
 import android.os.Bundle
 import android.os.Handler
 import android.os.Process
 import android.util.Log
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import java.io.ByteArrayOutputStream
+import java.nio.ByteBuffer
+import java.nio.ByteOrder
 import kotlin.concurrent.thread
 import kotlin.math.log10
+
 
 const val SAMPLING_RATE = 44100
 const val AUDIO_RECORDING = "SEGMENTATION"
@@ -18,23 +22,19 @@ const val AUDIO_RECORDING = "SEGMENTATION"
 class SegmentationActivity : AppCompatActivity() {
     var isRecording = false
 
-    private val shortPauseLength = 100
-    private val shortSpeechLength = 50
-
     private val audioBuffer: ShortArray
     private val countdownRecord: AudioRecord
     private val speechRecord: AudioRecord
     private val countdownVolumesList = mutableListOf<Double>()
     private val speechVolumesList = mutableListOf<Double>()
-    private var silenceLevel: Double? = null
-    private var isPause = false
     private var speechDuration: Long = 0
-    private var pauseStartTime: Long = 0
-    private val pausesCount = mutableListOf<Long>()
+    private val pauses = mutableListOf<Pair<Long, Boolean>>()
+    private val byteArrayOutputStream = ByteArrayOutputStream()
 
     init {
         var bufferSize = AudioRecord.getMinBufferSize(
-            SAMPLING_RATE, AudioFormat.CHANNEL_IN_MONO, AudioFormat.ENCODING_PCM_16BIT
+            SAMPLING_RATE, CHANNEL_IN_MONO,
+            ENCODING_PCM_16BIT
         )
         if (bufferSize == AudioRecord.ERROR || bufferSize == AudioRecord.ERROR_BAD_VALUE) {
             bufferSize = SAMPLING_RATE * 2
@@ -51,18 +51,21 @@ class SegmentationActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_segmentation)
 
-        val button = findViewById<Button>(R.id.start_button)
+        val startButton = findViewById<Button>(R.id.start_button)
 
-
-
-        button.setOnClickListener {
+        startButton.setOnClickListener {
             isRecording = !isRecording
-            button.text = if (isRecording) "Stop" else "Start"
+            startButton.text = if (isRecording) "Stop" else "Start"
 
             if (isRecording) {
                 startRecordSpeechAudio()
             }
         }
+
+        findViewById<Button>(R.id.play_button).setOnClickListener {
+            play()
+        }
+
 
         var cond = true
         Handler().postDelayed({
@@ -75,13 +78,31 @@ class SegmentationActivity : AppCompatActivity() {
     private fun initAudioRecording(bufferSize: Int): AudioRecord {
         val record = AudioRecord(
             MediaRecorder.AudioSource.MIC, SAMPLING_RATE,
-            AudioFormat.CHANNEL_IN_MONO, AudioFormat.ENCODING_PCM_16BIT, bufferSize
+            CHANNEL_IN_MONO, ENCODING_PCM_16BIT, bufferSize
         )
         if (record.state != AudioRecord.STATE_INITIALIZED) {
             throw Exception("Кажет нет разрешения")
         }
 
         return record
+    }
+
+    private fun play() {
+        val bufferSize = AudioRecord.getMinBufferSize(
+            SAMPLING_RATE, CHANNEL_CONFIGURATION_MONO,
+            ENCODING_PCM_16BIT
+        )
+
+        val audio = AudioTrack(
+            AudioManager.STREAM_MUSIC,
+            SAMPLING_RATE,
+            CHANNEL_CONFIGURATION_MONO,
+            ENCODING_PCM_16BIT,
+            bufferSize,
+            AudioTrack.MODE_STREAM
+        )
+        audio.play()
+        audio.write(byteArrayOutputStream.toByteArray(), 0, byteArrayOutputStream.toByteArray().size)
     }
 
     private fun recordCountdownAudio(continueCondition: () -> Boolean) {
@@ -104,7 +125,6 @@ class SegmentationActivity : AppCompatActivity() {
             Log.i(AUDIO_RECORDING, "Silence min level: ${volumeLevels.first}")
             Log.i(AUDIO_RECORDING, "Silence max level: ${volumeLevels.second}")
             Log.i(AUDIO_RECORDING, "Silence average level: ${volumeLevels.third}")
-
             Log.i(AUDIO_RECORDING, "Finished countdown audio recording")
         }
     }
@@ -126,13 +146,11 @@ class SegmentationActivity : AppCompatActivity() {
             Log.i(AUDIO_RECORDING, "Started speech audio recording")
 
             val startTime = System.currentTimeMillis()
-            var speechStartTime: Long = 0
             var totalFragmentsRead: Long = 0
-            var silentFragmentsRead: Long = 0
             var totalFragmentsOnSlide: Long = 0
-            var silentFragmentsOnSlide: Long = 0
 
-            silenceLevel = getAudioVolumeLevel(
+
+            val silenceLevel = getAudioVolumeLevel(
                 getAverageCountdownVolumeLevel(),
                 getMaximalCountdownVolumeLevel()
             )
@@ -141,30 +159,19 @@ class SegmentationActivity : AppCompatActivity() {
                 val shortsRead = speechRecord.read(audioBuffer, 0, audioBuffer.size)
                 val fragmentVolume = calculateVolume(shortsRead)
                 speechVolumesList.add(fragmentVolume)
-                if (fragmentVolume < silenceLevel!!) {
-                    if (!isPause) {
-                        isPause = true
-                        if ((System.currentTimeMillis() - speechStartTime) < shortSpeechLength) {
-                            // the pause is still continuing, so we remove the pause in the list
-                            // and later it will be replaced with the current, longer pause
-                            // which has the same start time
-                            pausesCount.dropLast(1)
-                        } else {
-                            pauseStartTime = System.currentTimeMillis()
-                        }
-                    }
-                    silentFragmentsRead++
-                    silentFragmentsOnSlide++
-                } else if (isPause) {
-                    val pauseLength = System.currentTimeMillis() - pauseStartTime
-                    if (pauseLength >= shortPauseLength) {
-                        pausesCount.add(pauseLength)
-                    }
-                    isPause = false
-                    speechStartTime = System.currentTimeMillis()
+                if (fragmentVolume < silenceLevel) {
+                    pauses.add(Pair(System.currentTimeMillis(), true))
+                } else {
+                    pauses.add(Pair(System.currentTimeMillis(), false))
                 }
                 totalFragmentsOnSlide++
                 totalFragmentsRead++
+
+                val bufferBytes = ByteBuffer.allocate(shortsRead * 2)
+                bufferBytes.order(ByteOrder.LITTLE_ENDIAN)
+                bufferBytes.asShortBuffer().put(audioBuffer, 0, shortsRead)
+                val bytes = bufferBytes.array()
+                byteArrayOutputStream.write(bytes)
             }
 
             speechDuration = System.currentTimeMillis() - startTime
