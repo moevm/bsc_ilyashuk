578cf8017f7cd1d97e3ac3ad8a7ee148a574a63d
cupofme <danil.ilyashuk@gmail.com>
2020-04-05 12:33:58 +0300

    Add basic ktor project

---
+++ backend/src/Application.kt
@@ -0,0 +1,46 @@
+package org.moevm.bsc_ilyashuk
+
+import io.ktor.application.*
+import io.ktor.response.*
+import io.ktor.routing.*
+import io.ktor.http.*
+import io.ktor.gson.*
+import io.ktor.features.*
+
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused")
+@kotlin.jvm.JvmOverloads
+fun Application.module(testing: Boolean = false) {
+    install(ContentNegotiation) {
+        gson {}
+    }
+
+    routing {
+        get("/") {
+            call.respondText("Hello diplom", contentType = ContentType.Text.Plain)
+        }
+
+        get("/json/gson") {
+            call.respond(mapOf("hello" to "world"))
+        }
+    }
+}
+
+//    val client = HttpClient() {
+//        install(JsonFeature) {
+//            serializer = GsonSerializer()
+//        }
+//    }
+//    runBlocking {
+//        // Sample for making a HTTP Client request
+//
+//        val message = client.post<JsonSampleClass> {
+//            url("http://127.0.0.1:8080/path/to/endpoint")
+//            contentType(ContentType.Application.Json)
+//            body = JsonSampleClass(hello = "world")
+//        }
+//    }
+
+//data class JsonSampleClass(val hello: String)
+
