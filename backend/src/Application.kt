c0680bff412d507334b88251ef25a1f61a44bd0f
cupofme <danil.ilyashuk@gmail.com>
2020-04-11 20:11:48 +0300

    Add tensorflow bindings

--- backend/src/Application.kt
+++ backend/src/Application.kt
@@ -6,6 +6,7 @@ import io.ktor.routing.*
 import io.ktor.http.*
 import io.ktor.gson.*
 import io.ktor.features.*
+import org.tensorflow.SavedModelBundle
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
@@ -15,12 +16,19 @@ fun Application.module(testing: Boolean = false) {
     install(ContentNegotiation) {
         gson {}
     }
+    val model = SavedModelBundle.load("saved_model", "serve")
 
     routing {
         get("/") {
+            val session = model.session()
+
+            model.graph().operations()
+
+            println(session.runner().runAndFetchMetadata().outputs)
+
+
             call.respondText("hello", contentType = ContentType.Text.Plain)
         }
-        
 
         get("/json/gson") {
             call.respond(mapOf("hello" to "world"))
