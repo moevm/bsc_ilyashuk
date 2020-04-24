f455e20def8149c323494dd4b65bba13a391417e
cupofme <danil.ilyashuk@gmail.com>
2020-04-24 20:00:29 +0300

    Group gender prediction

--- backend/src/Application.kt
+++ backend/src/Application.kt
@@ -27,19 +27,6 @@ fun Application.module(testing: Boolean = false) {
     }
     val model = SavedModelBundle.load("saved_model", "serve")
 
-    val labels = arrayOf(
-        "female_angry",
-        "female_calm",
-        "female_fearful",
-        "female_happy",
-        "female_sad",
-        "male_angry",
-        "male_calm",
-        "male_fearful",
-        "male_happy",
-        "male_sad"
-    )
-
     routing {
         get("/") {
             call.respondText("bsc_ilyashuk", contentType = ContentType.Text.Plain)
@@ -48,13 +35,13 @@ fun Application.module(testing: Boolean = false) {
         post("/predict") {
             val file = call.getFile()
 
-            val client = HttpClient() {
+            val client = HttpClient {
                 install(JsonFeature)
             }
 
             val features = client.get<Features>("http://localhost:5000?filename=${file.name}")
 
-            val predictions = ArrayList<FloatBuffer>()
+            val predictions = ArrayList<FloatArray>()
 
             for (i in 0 until features.data.size / 216) {
                 val session = model.session()
@@ -62,7 +49,7 @@ fun Application.module(testing: Boolean = false) {
 
                 val input = Tensor.create(
                     arrayOf<Long>(1, 216, 1).toLongArray(),
-                    FloatBuffer.wrap(features.data.slice((i * 216) until (((i + 1) * 216))).toFloatArray())
+                    FloatBuffer.wrap(features.data.slice((i * 216) until ((i + 1) * 216)).toFloatArray())
                 )
 
                 runner.apply {
@@ -72,10 +59,14 @@ fun Application.module(testing: Boolean = false) {
                 val outputTensor = runner.run()[0]
                 FloatBuffer.allocate(10).apply {
                     outputTensor.writeTo(this)
-                    predictions.add(this)
+                    val temp = FloatArray(5) { 0f }
+                    array().forEachIndexed { index, value ->
+                        temp[index % 5] += value
+                    }
+                    predictions.add(temp)
                 }
             }
-            call.respond(predictions.map { it.array() })
+            call.respond(predictions)
         }
     }
 }
--- frontend/src/labels.ts
+++ frontend/src/labels.ts
@@ -1,15 +1,4 @@
-export const labels = [
-  'female_angry',
-  'female_calm',
-  'female_fearful',
-  'female_happy',
-  'female_sad',
-  'male_angry',
-  'male_calm',
-  'male_fearful',
-  'male_happy',
-  'male_sad',
-];
+export const labels = ['angry', 'calm', 'fearful', 'happy', 'sad'];
 
 export const labelColors = [
   '#5899DA',
@@ -17,9 +6,9 @@ export const labelColors = [
   '#19A979',
   '#ED4A7B',
   '#945ECF',
-  '#13A4B4',
-  '#525DF4',
-  '#BF399E',
-  '#6C8893',
-  '#EE6868',
+  // '#13A4B4',
+  // '#525DF4',
+  // '#BF399E',
+  // '#6C8893',
+  // '#EE6868',
 ];
