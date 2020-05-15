768df384488f367da8db04e3fe68f76f2360782a
cupofme <danil.ilyashuk@gmail.com>
2020-05-15 14:14:47 +0300

    Delete files on backend

--- backend/src/Application.kt
+++ backend/src/Application.kt
@@ -7,6 +7,7 @@ import io.ktor.features.CORS
 import io.ktor.features.ContentNegotiation
 import io.ktor.gson.gson
 import io.ktor.http.ContentType
+import io.ktor.http.HttpStatusCode
 import io.ktor.response.respond
 import io.ktor.response.respondText
 import io.ktor.routing.get
@@ -37,35 +38,41 @@ fun Application.module(testing: Boolean = false) {
 
         post("/predict") {
             val file = call.getFile()
+            try {
+                val features = getFeaturesFromFile(file.name)
 
-            val features = getFeaturesFromFile(file.name)
+                val predictions = ArrayList<FloatArray>()
 
-            val predictions = ArrayList<FloatArray>()
+                for (i in 0 until features.data.size / 216) {
+                    val session = model.session()
+                    val runner = session.runner()
 
-            for (i in 0 until features.data.size / 216) {
-                val session = model.session()
-                val runner = session.runner()
+                    val input = Tensor.create(
+                        arrayOf<Long>(1, 216, 1).toLongArray(),
+                        FloatBuffer.wrap(features.data.slice((i * 216) until ((i + 1) * 216)).toFloatArray())
+                    )
 
-                val input = Tensor.create(
-                    arrayOf<Long>(1, 216, 1).toLongArray(),
-                    FloatBuffer.wrap(features.data.slice((i * 216) until ((i + 1) * 216)).toFloatArray())
-                )
-
-                runner.apply {
-                    feed("input_input", input)
-                    fetch("output/Softmax")
-                }
-                val outputTensor = runner.run()[0]
-                FloatBuffer.allocate(10).apply {
-                    outputTensor.writeTo(this)
-                    val temp = FloatArray(5) { 0f }
-                    array().forEachIndexed { index, value ->
-                        temp[index % 5] += value
+                    runner.apply {
+                        feed("input_input", input)
+                        fetch("output/Softmax")
                     }
-                    predictions.add(temp)
+                    val outputTensor = runner.run()[0]
+                    FloatBuffer.allocate(10).apply {
+                        outputTensor.writeTo(this)
+                        val temp = FloatArray(5) { 0f }
+                        array().forEachIndexed { index, value ->
+                            temp[index % 5] += value
+                        }
+                        predictions.add(temp)
+                    }
                 }
+                call.respond(predictions)
+
+            } catch (e: Exception) {
+                call.respond(HttpStatusCode.InternalServerError, "Error")
+            } finally {
+                file.delete()
             }
-            call.respond(predictions)
         }
     }
 }
