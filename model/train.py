2f8a638760ab5a74fd1bab2d501d56944da4c0ea
cupofme <danil.ilyashuk@gmail.com>
2020-05-22 16:56:53 +0300

    Train with pure keras

--- model/train.py
+++ model/train.py
@@ -1,12 +1,14 @@
 import os
+import shutil
+from pathlib import Path
 
 import joblib
+import keras
 import matplotlib.pyplot as plt
 import numpy as np
 import tensorflow as tf
 from sklearn.metrics import classification_report, confusion_matrix
 from sklearn.model_selection import train_test_split
-from tensorflow import keras
 
 from config import MODEL_DIR_PATH, SAVE_DIR_PATH
 
@@ -14,7 +16,7 @@ from config import MODEL_DIR_PATH, SAVE_DIR_PATH
 class TrainModel:
 
     @staticmethod
-    def train_neural_network(X, y) -> str:
+    def train_neural_network(X, y):
         X_train, X_test, y_train, y_test = train_test_split(
             X, y, test_size=0.33, random_state=42)
 
@@ -23,14 +25,14 @@ class TrainModel:
 
         print(x_traincnn.shape, x_testcnn.shape)
 
-        model = tf.keras.Sequential([
-            tf.keras.layers.Conv1D(64, 5, padding='same',
-                                   input_shape=(40, 1), name='input'),
-            tf.keras.layers.Activation('relu'),
-            tf.keras.layers.Dropout(0.2),
-            tf.keras.layers.Flatten(),
-            tf.keras.layers.Dense(8),
-            tf.keras.layers.Activation('softmax', name='output')
+        model = keras.Sequential([
+            keras.layers.Conv1D(64, 5, padding='same',
+                                input_shape=(40, 1), name='input'),
+            keras.layers.Activation('relu'),
+            keras.layers.Dropout(0.2),
+            keras.layers.Flatten(),
+            keras.layers.Dense(8),
+            keras.layers.Activation('softmax', name='output')
         ])
 
         print(model.summary)
@@ -43,24 +45,24 @@ class TrainModel:
                                 batch_size=16, epochs=50,
                                 validation_data=(x_testcnn, y_test))
 
-        # # Loss plotting
-        # plt.plot(cnn_history.history['loss'])
-        # plt.plot(cnn_history.history['val_loss'])
-        # plt.title('model loss')
-        # plt.ylabel('loss')
-        # plt.xlabel('epoch')
-        # plt.legend(['train', 'test'], loc='upper left')
-        # plt.savefig('loss.png')
-        # plt.close()
+        # Loss plotting
+        plt.plot(cnn_history.history['loss'])
+        plt.plot(cnn_history.history['val_loss'])
+        plt.title('model loss')
+        plt.ylabel('loss')
+        plt.xlabel('epoch')
+        plt.legend(['train', 'test'], loc='upper left')
+        plt.savefig('plots/loss.png')
+        plt.close()
 
-        # # Accuracy plotting
-        # plt.plot(cnn_history.history['accuracy'])
-        # plt.plot(cnn_history.history['val_accuracy'])
-        # plt.title('model accuracy')
-        # plt.ylabel('acc')
-        # plt.xlabel('epoch')
-        # plt.legend(['train', 'test'], loc='upper left')
-        # plt.savefig('accuracy.png')
+        # Accuracy plotting
+        plt.plot(cnn_history.history['accuracy'])
+        plt.plot(cnn_history.history['val_accuracy'])
+        plt.title('model accuracy')
+        plt.ylabel('acc')
+        plt.xlabel('epoch')
+        plt.legend(['train', 'test'], loc='upper left')
+        plt.savefig('plots/accuracy.png')
 
         predictions = model.predict_classes(x_testcnn)
         new_y_test = y_test.astype(int)
@@ -69,22 +71,22 @@ class TrainModel:
         print(classification_report(new_y_test, predictions))
         print(matrix)
 
-        model_name = 'model.h5'
+        # Remove model save folder if exists
+        dirpath = Path('model')
+        if dirpath.exists() and dirpath.is_dir():
+            shutil.rmtree(dirpath)
 
         # Save Keras model
         if not os.path.isdir(MODEL_DIR_PATH):
             os.makedirs(MODEL_DIR_PATH)
-        model_path = os.path.join(MODEL_DIR_PATH, model_name)
+        model_path = os.path.join(MODEL_DIR_PATH, 'model.h5')
         model.save(model_path)
-
-        print(model.input)
-        print(model.output)
 
         # Save in SavedModel format
         with tf.keras.backend.get_session() as sess:
             tf.saved_model.simple_save(
                 sess,
-                'saved_model',
+                'model/saved_model',
                 inputs={'input': model.input},
                 outputs={'output': model.output})
 
