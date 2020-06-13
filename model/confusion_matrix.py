70375388369907429dc32e70b74d276d13cbe14e
cupofme <danil.ilyashuk@gmail.com>
2020-06-13 11:30:25 +0300

    Add normalized confusion matrix calculation

--- model/confusion_matrix.py
+++ model/confusion_matrix.py
@@ -11,11 +11,9 @@ from sklearn.model_selection import train_test_split
 from config import MODEL_DIR_PATH, SAVE_DIR_PATH, classes_ru
 
 
-def plot_confusion_matrix(confusion_matrix, classes,
-                          normalize=False,
-                          title='Матрица ошибок',
-                          cmap=plt.cm.Blues):
-    plt.imshow(confusion_matrix, interpolation='nearest', cmap=cmap)
+def plot_confusion_matrix(confusion_matrix, classes, title, normalize=False):
+    plt.clf()
+    plt.imshow(confusion_matrix, interpolation='nearest', cmap=plt.cm.Blues)
     plt.title(title)
     plt.colorbar()
     tick_marks = np.arange(len(classes))
@@ -25,15 +23,10 @@ def plot_confusion_matrix(confusion_matrix, classes,
     if normalize:
         confusion_matrix = confusion_matrix.astype(
             'float') / confusion_matrix.sum(axis=1)[:, np.newaxis]
-        print("Normalized confusion matrix")
-    else:
-        print('Confusion matrix, without normalization')
-
-    print(confusion_matrix)
 
     thresh = confusion_matrix.max() / 2.
     for i, j in itertools.product(range(confusion_matrix.shape[0]), range(confusion_matrix.shape[1])):
-        plt.text(j, i, confusion_matrix[i, j],
+        plt.text(j, i, round(confusion_matrix[i, j], 3),
                  horizontalalignment="center",
                  color="white" if confusion_matrix[i, j] > thresh else "black")
 
@@ -54,8 +47,11 @@ if __name__ == '__main__':
     new_y_test = y.astype(int)
     matrix = confusion_matrix(new_y_test, predictions)
 
-    plot_confusion_matrix(matrix, classes=classes_ru)
+    plot_confusion_matrix(matrix, classes=classes_ru, title="Матрица ошибок")
     plt.savefig('plots/confusion_matrix.png')
-    plt.show()
+
+    plot_confusion_matrix(matrix, classes=classes_ru,
+                          normalize=True, title="Нормализованная матрица ошибок")
+    plt.savefig('plots/normalized_confusion_matrix.png')
 
     print(classification_report(new_y_test, predictions))
