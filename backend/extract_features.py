a1551165fd131393d150c96128b7dcbb3a47337e
cupofme <danil.ilyashuk@gmail.com>
2020-06-14 12:46:25 +0300

    Change chunk splitting

--- backend/extract_features.py
+++ backend/extract_features.py
@@ -1,4 +1,5 @@
 import json
+import math
 import os
 import sys
 
@@ -6,27 +7,42 @@ import librosa
 import numpy as np
 from pydub import AudioSegment
 
-filename = './uploads/' + sys.argv[1]
-chunk_length = float(sys.argv[2])
 
-converted = AudioSegment.from_file(filename).export("temp.wav", format="wav")
+def split_into_chunks(array, chunk_size):
+    chunks = []
+    number_of_chunks = math.ceil((len(array) / chunk_size))
+    for i in range(number_of_chunks):
+        start_index = math.ceil(i * chunk_size)
+        end_index = math.ceil((i + 1) * chunk_size)
+        chunks.append(array[start_index: end_index])
 
-filename = converted.name
+    return chunks
 
-sr = librosa.core.get_samplerate(filename)
 
-duration = librosa.core.get_duration(filename=filename, sr=sr)
+if __name__ == "__main__":
+    filename = './uploads/' + sys.argv[1]
+    chunk_length = float(sys.argv[2])
 
-data, sr = librosa.load(filename, res_type='kaiser_fast', sr=sr)
+    converted = AudioSegment.from_file(
+        filename).export("temp.wav", format="wav")
 
-raw_mfccs = librosa.feature.mfcc(y=data,
-                                 sr=sr,
-                                 n_mfcc=40).T
+    filename = converted.name
 
-mfcc_chunks = np.array_split(raw_mfccs, duration / chunk_length)
+    sr = librosa.core.get_samplerate(filename)
 
-mean = [np.mean(chunk, axis=0).tolist() for chunk in mfcc_chunks]
+    duration = librosa.core.get_duration(filename=filename, sr=sr)
 
-os.remove(converted.name)
+    data, sr = librosa.load(filename, res_type='kaiser_fast', sr=sr)
 
-print(json.dumps({"data": mean}))
+    raw_mfccs = librosa.feature.mfcc(y=data,
+                                     sr=sr,
+                                     n_mfcc=40).T
+
+    mfcc_per_second = len(raw_mfccs) / duration
+
+    mfcc_chunks = split_into_chunks(raw_mfccs, mfcc_per_second * chunk_length)
+
+    mean = [np.mean(chunk, axis=0).tolist() for chunk in mfcc_chunks]
+
+    os.remove(converted.name)
+    print(json.dumps({"data": mean}))
