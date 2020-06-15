295de8f2ca7f266fad8aa543aab80e4de7a31538
cupofme <danil.ilyashuk@gmail.com>
2020-06-15 19:02:16 +0300

    Fix emotions chart

--- frontend/src/controllers/MainPage/subcontrollers/EmotionsChartController.ts
+++ frontend/src/controllers/MainPage/subcontrollers/EmotionsChartController.ts
@@ -17,14 +17,29 @@ export default class EmotionsChartController {
   @action.bound
   public formData = (predictions: TimePrediction[]) => {
     const res: any[] = [];
+
+    var zeroChartData: any = { time: 0 };
+    labels.forEach((label) => {
+      zeroChartData[label] = 0;
+    });
+    res.push(zeroChartData);
     predictions.forEach((prediction: TimePrediction) => {
       var chartData: any = {};
-      chartData.time = prediction.timeFrom;
+      chartData.time = ((prediction.timeTo + prediction.timeFrom) / 2).toFixed(
+        2
+      );
       labels.forEach((label, labelIndex) => {
         chartData[label] = prediction.prediction[labelIndex].toFixed(3);
       });
       res.push(chartData);
     });
+    zeroChartData = {
+      time: predictions[predictions.length - 1].timeTo.toFixed(2),
+    };
+    labels.forEach((label) => {
+      zeroChartData[label] = 0;
+    });
+    res.push(zeroChartData);
     this.chartData = res;
   };
 }
